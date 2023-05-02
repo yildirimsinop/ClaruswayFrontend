4936d302b9eaac2ecfeb38a55e20915454293252
Umit Yildirim <umity@uia.no>
2023-05-02 21:28:18 +0200

    error

---
+++ js01-intro/app.js
@@ -0,0 +1,12 @@
+//* Javascript Kodu */
+console.log("Localde bulunan script calisti")
+
+alert("Bu bir alert dir") //? Alert uygulamayi bloklar. Yani uygulamayi keser.
+
+karar = confirm ("Emin misiniz E/H") // Bunlar bir (Blocking Code'dir. alert ve confirm gibi)
+console.log(karar)
+
+console.warn ("Bu bir uyaridir")
+
+console.error ("Eyvah hata olustu")
+
