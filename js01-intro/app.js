ae9aae0babdc6cdfc144356e720cda78393d0510
Umit <yildirimsinop>
2023-05-02 22:18:10 +0200

    siste

--- js01-intro/app.js
+++ js01-intro/app.js
@@ -1,38 +1,39 @@
-// //* Javascript Kodu */
+//* Javascript Kodu */
 // console.log("Localde bulunan script calisti")
 
-// alert("Bu bir alert dir") //? Alert uygulamayi bloklar. Yani uygulamayi keser.
+// alert("Bu bir alert dir") /* Alert uygulmayı bloklar (Blocking Code)*/
 
-// karar = confirm ("Emin misiniz E/H") // Bunlar bir (Blocking Code'dir. alert ve confirm gibi)
+// karar = confirm("Emin msiniz E/H")
 // console.log(karar)
 
-// console.warn ("Bu bir uyaridir")
+// console.warn("Bu bir uyarıdir")
 
-// console.error ("Eyvah hata olustu")
+// console.error("Eyvah hata olustu..")
 
-console.log ("APP JS RUNNING")
+//*  CONST ve LET, ECMASCRIPT6 ile gelmiştir. VAR eski versiyonlardan beri bulunmaktadır.
+
+//? ===================  CONST  ======================
+
+console.log("APP JS RUNNING")
 
 const pi = 3.14
-console.log (pi)
-console.log(typeof pi);
+console.log(pi)
+console.log(typeof pi)
 
 const sayi1 = "3"
-console.log(typeof sayi1);
+console.log(typeof sayi1)
 
-// ! Bu bir hata
-// const deneme
-// console.log(denem);
+//! caught SyntaxError: Missing initializer in const declaration (at app.js:27:7)
+// const deneme; //? const ile tanimlanan degiskene baslangic degeri verilmelidir.
+// console.log(deneme)
 
-// 
-//! const olusturulmus bir degiskene sonradan atama yapilamaz
+//! Uncaught TypeError: Assignment to constant variable.
 // const number1 = 5
-// number1 = 7
-
+// number1 = 7 //? const ile tanimlanan degiskene sonradan atama yapilamaz
 
 const varMi = true
 const isOpen = false
-
-console.log(varMi, typeof varMi);
+console.log(isOpen, typeof isOpen)
 
 //? ======================  LET  ========================
 //* LET de CONST gibi yaygın kullanılan değişken tanımlama yöntemidir.
@@ -41,29 +42,29 @@ console.log(varMi, typeof varMi);
 //* CONST kullanamadığımız durumlarda (değişkenin değeri sürekli değişecekse) LET kullanmalıyız.
 
 let language = "Java"
-console.log(language, typeof language);
+console.log(language, typeof language)
 
 language = "Javascript"
-console.log(language);
+console.log(language)
 
-language = true
-console.log(language);
+language = true //? atama
+console.log(language)
 
 language = 3.14
-console.log(language);
+console.log(language)
 
-let adet; // Js bir degiskenin turunu atanan degere gore belirler. Eger atama olmadiysa turu undefined olarak belirlenmis olur.
-console.log (adet) // undefined
+let adet //? Js bir degiskenin türünü atanan degere gore belirler. Eger atama olmadıysa türu undefined olarak belirlenmiş olur.
+console.log(adet) //?undefined
 
-adet = 23
-console.log(adet);
+adet = "23" //? atama
+console.log(typeof adet)
 
 {
-    // Bir blok icerisinde degisken olusturduk
-    let localDegisken = 5
-    localDegisken = localDegisken + 1
-    console.log(localDegisken);
+  //? Bir blok icerisinde degisken olustruduk
+  let localDegisken = 5
+  localDegisken = localDegisken + 1
+  console.log(localDegisken)
 }
 
-console.log(localDegisken);
-
+//! Uncaught ReferenceError: localDegisken is not defined
+console.log(localDegisken)
