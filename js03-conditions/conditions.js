b20281d58340dc18a60b5e28dd8202e532c02c07
Umit <yildirimsinop>
2023-05-08 22:41:11 +0200

    js

--- js03-conditions/conditions.js
+++ js03-conditions/conditions.js
@@ -91,31 +91,57 @@ console.log("****** CONDITIONS *******")
 
 //**************** TERNARY *******************
 
-const grade1 = prompt("Please enter your grade:")
+// const grade1 = prompt("Please enter your grade:")
 
 // grade1 >= 50 ? console.log("SUCCEED") : console.log("FAILED")
 // const score = grade1 >= 50 ? "PASSED" : "FAILED"
 // console.log(score)
 
-//? tek durumlu kosullandirmada da mecburen else yapisi (: ve sonrasi) kullanilmak zorundadir.
-const score1 = grade1 >= 50 ? "SUCCEED" : ""
-console.log(score1)
+// //? tek durumlu kosullandirmada da mecburen else yapisi (: ve sonrasi) kullanilmak zorundadir.
+// const score1 = grade1 >= 50 ? "SUCCEED" : ""
+// console.log(score1)
 
-//? Nested ternary yazmak aslinda anlasilabilirligi azaltiyor. Bu sebeple, 2 durum var ise
-//? Ternary kullanmak daha mantiklidir
-const speed = 110
+// //? Nested ternary yazmak aslinda anlasilabilirligi azaltiyor. Bu sebeple, 2 durum var ise
+// //? Ternary kullanmak daha mantiklidir
+// const speed = 110
 
-speed > 120
-  ? console.log("Speedy")
-  : speed >= 90
-  ? console.log("Normal")
-  : console.log("Low Speed")
+// speed > 120
+//   ? console.log("Speedy")
+//   : speed >= 90
+//   ? console.log("Normal")
+//   : console.log("Low Speed")
 
-//? ayni örneğin if-else ile yapilmasi
-if (speed > 120) {
-  console.log("Speedy")
-} else if (speed >= 90) {
-  console.log("Normal")
-} else {
-  console.log("Low Speed")
-}
+// //? ayni örneğin if-else ile yapilmasi
+// if (speed > 120) {
+//   console.log("Speedy")
+// } else if (speed >= 90) {
+//   console.log("Normal")
+// } else {
+//   console.log("Low Speed")
+// }
+
+//**************** SHORT-CIRCUIT *******************
+
+const age = 18
+
+let description = ""
+//? if-else
+// if (age >= 18) {
+//   description = "Adult"
+// }
+
+//? Ternary
+// description = age >= 18 ? "Adult" : ""
+
+//? Short-Circuit (Tek bir durum var ise short-circuit yontemi daha kisa oluyor)
+//* age 18'e eşit veya daha büyükse (true) description'a Adult aktar aksi takdirde hiç bir şey yapma
+age >= 18 && (description = "Adult")
+
+//* age 18'e eşit veya daha büyük değilse (false) description'a Adult aktar aksi takdirde hiç bir şey yapma
+age < 18 || (description = "Adult")
+
+console.log(description)
+
+//! ODEV1: Dort Islem Hesap Makinasi
+
+//**************** SWITCH-CASE *******************
