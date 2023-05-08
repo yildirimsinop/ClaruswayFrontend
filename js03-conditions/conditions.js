ad89e3cf3beddd47846465b97c427d24cc970b7c
Umit <yildirimsinop>
2023-05-08 21:53:53 +0200

    ternary

--- js03-conditions/conditions.js
+++ js03-conditions/conditions.js
@@ -25,66 +25,97 @@ console.log("****** CONDITIONS *******")
 // }
 // console.log("Güle güle")
 
-//? Cok kosullu
-const grade = prompt("Please enter your grade:")
+// //? Cok kosullu
+// const grade = prompt("Please enter your grade:")
 
-let result = null
-// let result = ""
-if (grade < 0) {
-  console.log("Grade can not be smaller than zero")
-} else if (grade < 40) {
-  result = "FF"
-} else if (grade <= 50) {
-  result = "DD"
-} else if (grade <= 65) {
-  result = "CC"
-} else if (grade <= 89) {
-  result = "BB"
-} else if (grade <= 100) {
-  result = "AA"
-} else {
-  console.log("Grade can not be bigger than 100")
-}
-
-if (result != null) {
-  console.log(`Your score:${result}`)
-}
-//? Kısa yol
-if (result) {
-  console.log(`Your score:${result}`)
-}
-
-//? ORNEK2: console'dan 3 tamsayi alarak bunlarin en buyugunu
-//? yazdiriniz.
-
-const n1 = +prompt("Num1:")
-const n2 = +prompt("Num2:")
-const n3 = +prompt("Num3:")
-
-//* 1.YONTEM
-//? nested if
-// if (n1 > n2) {
-//   if (n1 > n3) {
-//     console.log("Biggest:", n1)
-//   }
+// let result = null
+// // let result = ""
+// if (grade < 0) {
+//   console.log("Grade can not be smaller than zero")
+// } else if (grade < 40) {
+//   result = "FF"
+// } else if (grade <= 50) {
+//   result = "DD"
+// } else if (grade <= 65) {
+//   result = "CC"
+// } else if (grade <= 89) {
+//   result = "BB"
+// } else if (grade <= 100) {
+//   result = "AA"
+// } else {
+//   console.log("Grade can not be bigger than 100")
 // }
 
-//? kosullarin logical operatorler yardimiyla birleştirilmesi
-if (n1 > n2 && n1 > n3) {
-  console.log("Biggest:", n1)
-} else if (n2 > n1 && n2 > n3) {
-  console.log("Biggest:", n2)
-} else if (n3 > n1 && n3 > n2) {
-  console.log("Biggest:", n3)
-}
+// if (result != null) {
+//   console.log(`Your score:${result}`)
+// }
+// //? Kısa yol
+// if (result) {
+//   console.log(`Your score:${result}`)
+// }
 
-//* 2.YONTEM
-let biggest = n1
+// //? ORNEK2: console'dan 3 tamsayi alarak bunlarin en buyugunu
+// //? yazdiriniz.
 
-if (n2 >= biggest) {
-  biggest = n2
+// const n1 = +prompt("Num1:")
+// const n2 = +prompt("Num2:")
+// const n3 = +prompt("Num3:")
+
+// //* 1.YONTEM
+// //? nested if
+// // if (n1 > n2) {
+// //   if (n1 > n3) {
+// //     console.log("Biggest:", n1)
+// //   }
+// // }
+
+// //? kosullarin logical operatorler yardimiyla birleştirilmesi
+// if (n1 >= n2 && n1 >= n3) {
+//   console.log("Biggest:", n1)
+// } else if (n2 >= n1 && n2 >= n3) {
+//   console.log("Biggest:", n2)
+// } else if (n3 >= n1 && n3 >= n2) {
+//   console.log("Biggest:", n3)
+// }
+
+// //* 2.YONTEM
+// let biggest = n1
+
+// if (n2 >= biggest) {
+//   biggest = n2
+// }
+// if (n3 >= biggest) {
+//   biggest = n3
+// }
+// console.log(`${biggest} is biggest`)
+
+//**************** TERNARY *******************
+
+const grade1 = prompt("Please enter your grade:")
+
+// grade1 >= 50 ? console.log("SUCCEED") : console.log("FAILED")
+// const score = grade1 >= 50 ? "PASSED" : "FAILED"
+// console.log(score)
+
+//? tek durumlu kosullandirmada da mecburen else yapisi (: ve sonrasi) kullanilmak zorundadir.
+const score1 = grade1 >= 50 ? "SUCCEED" : ""
+console.log(score1)
+
+//? Nested ternary yazmak aslinda anlasilabilirligi azaltiyor. Bu sebeple, 2 durum var ise
+//? Ternary kullanmak daha mantiklidir
+const speed = 110
+
+speed > 120
+  ? console.log("Speedy")
+  : speed >= 90
+  ? console.log("Normal")
+  : console.log("Low Speed")
+
+//? ayni örneğin if-else ile yapilmasi
+if (speed > 120) {
+  console.log("Speedy")
+} else if (speed >= 90) {
+  console.log("Normal")
+} else {
+  console.log("Low Speed")
 }
-if (n3 >= biggest) {
-  biggest = n3
-}
-console.log(`${biggest} is biggest`)
