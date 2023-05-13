18247b8a887beba531cca40eee1d49b2605f1725
Umit Yildirim <umity@uia.no>
2023-05-13 13:20:41 +0200

    arrow

--- JS05_functions1/expression.js
+++ JS05_functions1/expression.js
@@ -35,3 +35,37 @@ let oddEven = function (number) {
 
 console.log(oddEven(5))
 console.log(oddEven(2))
+
+//* ORNEK3: 3 sayinin en buyugunu bulan fonks.
+//***********************************************/
+
+//? Arrow
+
+const kareAl = (num) => num * num
+const topla = (s1, s2) => s1 + s2
+const ciktiVer = () => console.log("Merhaba")
+
+console.log("KARE:", kareAl(4))
+console.log("TOPLA:", topla(4, 5))
+ciktiVer()
+
+//* ORNEK4: 3 sayinin en buyugunu bulan fonks.
+//***********************************************/
+//! Süslü parantez kullanıldığnda otomatik returning ozelligi kaybolur
+const findBiggest = (n1, n2, n3) => {
+  let biggest = n1
+
+  if (n2 >= biggest) {
+    biggest = n2
+  }
+  if (n3 >= biggest) {
+    biggest = n3
+  }
+  return biggest
+}
+
+console.log("BIGGEST:", findBiggest(1, 3, 2))
+
+//? Alternatif olarak
+const findBiggestShort = (num1, num2, num3) => Math.max(num1, num2, num3)
+console.log("BIGGEST:", findBiggestShort(1, 3, 2))
