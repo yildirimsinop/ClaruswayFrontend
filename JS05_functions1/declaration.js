2f60f22b21fd246d1c4dbfd51a8c7f6ffa270b77
Umit <yildirimsinop>
2023-05-13 12:04:08 +0200

    fonksjon

--- JS05_functions1/declaration.js
+++ JS05_functions1/declaration.js
@@ -4,14 +4,75 @@
 
 console.log("** FUNC DECLARATION **")
 
+//* ORNEK1:
+//*****************************************/
 
-
-// ! Declaration (Fonksyonun tanimlanmasi)
-
-function yazdirMerhaba () {
-   alert ("Merhabe televole");
+//! Declaration (Fonksiyonun tanimlanmasi)
+function yazdirMerhaba() {
+  // alert("Merhaba FS14")
+  console.log("Merhaba FS14")
 }
 
+yazdirMerhaba() //! invoke, call
+yazdirMerhaba() //! invoke, call
+yazdirMerhaba() //! invoke, call
 
-yazdirMerhaba ()
+//* ORNEK2:
+//*****************************************/
 
+//! Parametre listesi (argüman listesi)
+function yazdirParametre(message, name = "Cohort14") {
+  console.log(message, name)
+}
+yazdirParametre("Hello", "FS15")
+yazdirParametre("Salut", "FS16")
+yazdirParametre("Hola", "FS17")
+yazdirParametre(12.4, true)
+
+//! Bir parametreyi cagirma sirasinda kullanmaz isek onun
+//! yerine default parametre atayabiliriz. Ornekteki name
+//! parametresi icin default değer olarak 'Cohort14' atanmistir.
+
+yazdirParametre("Hallo")
+yazdirParametre("Ehlen")
+yazdirParametre()
+
+//* ORNEK3:
+//******************************************/
+console.log(new Date()) //? Sistem tarihi ve saatini verir.
+
+function calculate(name, yob) {
+  const age = new Date().getFullYear() - yob
+  // console.log(`${name} is ${age} years old`)
+  return age
+}
+
+console.log(calculate("Ahmet", 1990))
+const ismetAge = calculate("İsmet", 1890)
+console.log("Ismet's Age:", ismetAge)
+
+//* ORNEK4:
+//******************************************/
+
+function add(num1, num2) {
+  const sum = num1 + num2
+  return sum
+}
+
+function addShort(num1, num2) {
+  return (num1 + num2) / (num1 * num2)
+}
+
+const result1 = add(3, 5)
+const result2 = add(5, 9)
+const result3 = addShort(5, 9)
+
+console.log(result1, result2)
+console.log(result3)
+
+//* ORNEK5:
+//*******************************************/
+
+//? Console'dan girilen bir sayinin tek veya cift olddgunu
+//? hesaplayan ve sonucu ana programa donduren kodu fonksiyonlar
+//? ile yaziniz.
