3e254aa0f0f5f47a9b8850d34913d5d325cb3f08
Umit Yildirim <umity@uia.no>
2023-05-13 10:47:38 +0200

    fonk

---
+++ JS05_functions1/declaration.js
@@ -0,0 +1,17 @@
+// ? =====================================
+// ?           FUNCTIONS
+// ? ======================================
+
+console.log("** FUNC DECLARATION **")
+
+
+
+// ! Declaration (Fonksyonun tanimlanmasi)
+
+function yazdirMerhaba () {
+   alert ("Merhabe televole");
+}
+
+
+yazdirMerhaba ()
+
