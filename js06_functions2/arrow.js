cdeace3eeb0c70223a12e865197eb11bc75c1e30
Umit <yildirimsinop>
2023-05-15 20:38:55 +0200

    nested fonks

--- js06_functions2/arrow.js
+++ js06_functions2/arrow.js
@@ -50,22 +50,26 @@ console.log(`AGE:${calculateAge(1990)}`)
 //******************************************/
 //? hacim = Pi x r x r x h
 
-const h = Number(prompt("h:"))
-const r = Number(prompt("r:"))
-// const pi = 3.14
+// const h = Number(prompt("h:"))
+// const r = Number(prompt("r:"))
+// // const pi = 3.14
 
-const volume = (r, h) => Math.PI * r * r * h
-console.log("VOLUME:", volume(r, h).toFixed(2))
-console.log("VOLUME:", volume(r, h).toPrecision(5))
+// const volume = (r, h) => Math.PI * r * r * h
+// console.log("VOLUME:", volume(r, h).toFixed(2))
+// console.log("VOLUME:", volume(r, h).toPrecision(5))
 
-//? toPrecision(5) aldigi parametreye gore bir sayinin tam ve kesir
-//? kisimlarinin ne kadarini gosterilecegini belirlemek icin
-//? kullanilabilir. 5 degeri tam ve kesir icin toplam 5 basamak kullan demektir.
-console.log("VOLUME:", volume(r, h).toExponential(5))
-//? toExponential() ise bir sayinin bilimsel (10 uzeri) olarak
+// //? toPrecision(5) aldigi parametreye gore bir sayinin tam ve kesir
+// //? kisimlarinin ne kadarini gosterilecegini belirlemek icin
+// //? kullanilabilir. 5 degeri tam ve kesir icin toplam 5 basamak kullan demektir.
+// console.log("VOLUME:", volume(r, h).toExponential(5))
+// //? toExponential() ise bir sayinin bilimsel (10 uzeri) olarak
 
 //* ORNEK: Bir fonksiyon icerisinde baska fonksiyonlar cagrilabilir (Hesap Makinası)
 //**************************************************/
+const add = (n1, n2) => n1 + n2
+const sub = (n1, n2) => n1 - n2
+const mul = (n1, n2) => n1 * n2
+const div = (n1, n2) => n1 / n2
 
 const compute = (n1, n2, operator) => {
   let result = 0
@@ -75,15 +79,26 @@ const compute = (n1, n2, operator) => {
       result = add(n1, n2)
       break
     case "-":
+      result = sub(n1, n2)
       break
-
+    case "x":
+    case "*":
+      result = mul(n1, n2)
+      break
+    case "/":
+      result = div(n1, n2)
+      break
     default:
+        alert("Incorrect Operator")
+        result = ""
       break
   }
+
+  return result
 }
 
 const n1 = +prompt("First Number:")
-const n2 = +prompt("Second Number:")
 const operator = prompt("Operator:")
+const n2 = +prompt("Second Number:")
 
-console.log(compute(n1, n2, operator))
+console.log(`${n1}${operator}${n2}=${compute(n1, n2, operator)}`)
