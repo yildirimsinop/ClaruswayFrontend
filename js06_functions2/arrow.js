eaca9883463d935e1793da7f79eaa3ae01dc5586
Umit <yildirimsinop>
2023-05-15 21:02:42 +0200

    fakto

--- js06_functions2/arrow.js
+++ js06_functions2/arrow.js
@@ -66,39 +66,62 @@ console.log(`AGE:${calculateAge(1990)}`)
 
 //* ORNEK: Bir fonksiyon icerisinde baska fonksiyonlar cagrilabilir (Hesap Makinası)
 //**************************************************/
-const add = (n1, n2) => n1 + n2
-const sub = (n1, n2) => n1 - n2
-const mul = (n1, n2) => n1 * n2
-const div = (n1, n2) => n1 / n2
+// const add = (n1, n2) => n1 + n2
+// const sub = (n1, n2) => n1 - n2
+// const mul = (n1, n2) => n1 * n2
+// const div = (n1, n2) => n1 / n2
 
-const compute = (n1, n2, operator) => {
-  let result = 0
-  switch (operator) {
-    case "+":
-      //   result = n1 + n2
-      result = add(n1, n2)
-      break
-    case "-":
-      result = sub(n1, n2)
-      break
-    case "x":
-    case "*":
-      result = mul(n1, n2)
-      break
-    case "/":
-      result = div(n1, n2)
-      break
-    default:
-        alert("Incorrect Operator")
-        result = ""
-      break
-  }
+// const compute = (n1, n2, operator) => {
+//   let result = 0
+//   switch (operator) {
+//     case "+":
+//       //   result = n1 + n2
+//       result = add(n1, n2)
+//       break
+//     case "-":
+//       result = sub(n1, n2)
+//       break
+//     case "x":
+//     case "*":
+//       result = mul(n1, n2)
+//       break
+//     case "/":
+//       result = div(n1, n2)
+//       break
+//     default:
+//         alert("Incorrect Operator")
+//         result = ""
+//       break
+//   }
 
-  return result
+//   return result
+// }
+
+// const n1 = +prompt("First Number:")
+// const operator = prompt("Operator:")
+// const n2 = +prompt("Second Number:")
+
+// console.log(`${n1}${operator}${n2}=${compute(n1, n2, operator)}`)
+
+//* ORNEK: Girilen sayinin faktoriyelini hesaplayan programı fonksiyonlar yardımıyla kodlayiniz.
+//? FIBONACCI terimleri:  n! = n * (n-1)!
+//? Fakto(5) => 5.4.3.2
+
+const fakto = (n) => {
+
+    let f = 1
+
+    for (let i = n; i >1; i--){
+
+        f += f * i
+
+    } 
+
+    return f
+    
+
 }
 
-const n1 = +prompt("First Number:")
-const operator = prompt("Operator:")
-const n2 = +prompt("Second Number:")
+const number =Number(prompt ("n :"))
 
-console.log(`${n1}${operator}${n2}=${compute(n1, n2, operator)}`)
+console.log(`Fakto (${number})= ${fakto(number)}`);
