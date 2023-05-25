a038c99eb4e88ab0551417a45e55f8f08be968a7
Umit <yildirimsinop>
2023-05-25 22:16:14 +0200

    for of

--- Js/js11_new-gen-operators/newOps.js
+++ Js/js11_new-gen-operators/newOps.js
@@ -92,7 +92,9 @@ team.forEach((person) => {
   console.log("AGE:", age)
   console.log("**************")
 })
+//!----  FUNCTIONLARDA DEATRUC. KULLANIMI ----
 
+//? Donus degeri olarak
 const getProduct = () => {
   return {
     id: "123",
@@ -111,6 +113,7 @@ console.log("PRODUCT NAME:", pName, "PRICE:", price)
 stock = stock - 100
 console.log("REMAINING STOCK:", stock)
 
+//? Parametre olarak
 const data = {
   id: "123",
   pName: "NIKE",
@@ -121,8 +124,203 @@ const data = {
 //   console.log(pName, price)
 // }
 
-const print = function ({ pName, price }) {  //? Yolda destr.
+const print = function ({ pName, price }) {
+  //? Yolda destr.
   console.log(pName, price)
 }
 
 print(data)
+
+//* ======================================================
+//*  DESTRUCTURING (ARRAY)
+//* ======================================================
+const names = ["Ahmet", "Mehmet", "İsmet", "Saffet"]
+
+//? Klasik
+const ismet = names[2]
+console.log(ismet)
+
+const [ahmo, , ismo, safo] = names
+
+console.log(ahmo, ismo, safo)
+
+//*======================================================
+//*  REST (...)
+//* ======================================================
+
+//? REST operatoru kullanici tarafindan girilen degerleri dizi
+//? icerisine konumlandirir. Cesitli kullanim alanlari vardir.
+
+//! 1- Bir dizi veya object'deki bazi degerlerden geri kalanlarini
+//!    ayri dizi yada objelere kopyalanmasini saglayabilir.
+
+//* REST: Array
+const araclar = ["bmw", "merso", "ferarri", "rover", "lambo"]
+
+const [araba1, araba2, araba3, ...geriKalanlar] = araclar
+console.log(araba1)
+console.log(geriKalanlar)
+
+//* REST: Object
+
+const kisi = {
+  ad: "Canan",
+  soyad: "Cano",
+  yas: 33,
+  meslek: "tester",
+}
+
+const { meslek, yas, ...isim } = kisi
+
+console.log(meslek, yas)
+console.log(isim)
+console.log(kisi)
+
+//! 2- Bir fonksiyonun argumanlarini diziye cevirmek icin kullanilabilir.
+
+const topla = (a, b) => a + b
+
+console.log("SONUC:", topla(2, 5, 10, 4))
+
+//? REST (...) ile non-iterable olan sayilari iterable hale (diziye)
+//? cevirmiş olduk.
+
+const toplaminiAl = (...sayilar) => {
+  console.log(sayilar)
+  return sayilar.reduce((t, v) => t + v, 0)
+}
+console.log("SONUC:", toplaminiAl(2, 5, 10, 4, 6, 8))
+
+//*==================================================
+//*  SPREAD (...)
+//* =================================================
+
+//? Spread operatoru ise iterables olan bir elemani bireysel
+//? degerler haline getirir.
+
+const ucanAraclar = ["drone", "heli", "ucak"]
+const karaAraclari = ["Tir", "Araba", "Kamyonet", "Bisiklet"]
+
+const tasitlar = [ucanAraclar, karaAraclari]
+console.log(tasitlar)
+console.log(tasitlar[0][1])
+
+const tasitlar1 = [...karaAraclari, "Kamyon", ...ucanAraclar]
+console.log(tasitlar1)
+
+const cumle = "Olmak ya da olmamak"
+
+const karakter = [...cumle]
+console.log(karakter)
+console.log(cumle)
+
+console.log("MAX:", Math.max(1, 3, 4, -1, 4))
+const rakamlar = [23, 45, 66, 77, 12, -1]
+
+//? MATH.MIN
+console.log("MIN:", Math.min(...rakamlar))
+
+//? Array Copy
+const myNumbers = [4, 5, 6, [11, 22]]
+const herNumbers = [1, 2, 3, ...myNumbers]
+const hisNumbers = [...herNumbers]
+
+console.log(herNumbers, myNumbers)
+console.log(hisNumbers)
+
+hisNumbers.push(7)
+myNumbers.push(77)
+
+console.log("MY:", myNumbers)
+console.log("HER:", herNumbers)
+console.log("HIS:", hisNumbers)
+
+myNumbers[3][1] = 44 //? spread kullanılsa dahi nested olan alanlar deep copy olmaz.
+
+//? Sıg Kopyalama
+const theirNumbers = hisNumbers
+
+theirNumbers.push(8)
+console.log(theirNumbers, hisNumbers)
+
+//? Object copying
+
+const firstObj = { a: 1, b: 2, c: 3 }
+const secondObj = { a: 2, d: 3, c: 4 }
+
+const copiedFirstObj = { ...firstObj }
+console.log(copiedFirstObj)
+
+copiedFirstObj.a = 44
+//? nesting olmadigi icin deep copy gibi davranir.
+console.log(copiedFirstObj, firstObj)
+
+const combinedObjs = { ...secondObj, ...firstObj }
+console.log(combinedObjs)
+
+//? nested
+const people = {
+  person1: {
+    name: "Can",
+    surname: "Canan",
+    dob: "1990",
+    job: "developer",
+    salary: "140000",
+    drivingLicense: true,
+  },
+  person2: {
+    name: "John",
+    surname: "Sweet",
+    dob: "1990",
+    job: "tester",
+    salary: "110000",
+    drivingLicense: false,
+  },
+  person3: {
+    name: "Steve",
+    surname: "Job",
+    dob: "2000",
+    job: "developer",
+    salary: "90000",
+    drivingLicense: true,
+  },
+}
+
+console.log("MAAAS:", people.person3.salary)
+
+//! FOR - IN
+//* for (key in object) {
+//*   // code block to be executed
+//* }
+
+for (let p in people) {
+  // console.log(p)
+  // console.log(people[p]) //? square bracket notasyon
+  console.log(people[p].salary) //? square bracket notasyon
+}
+
+//? Javascript'de Objeler default olarak iterable degildir.
+//? Ama for in ve for of donguleri ile itere edilebilirler.
+
+//? Objelerin key ve value'larini okumak icin built-in metotlar vardir.
+//? Bu mettotlar aslinda objelerin key ve/veya value'lari bir dizi olarak dondurur.
+console.log(Object.keys(people))
+console.log(Object.values(people))
+console.log(Object.entries(people))
+
+//! FOR - OF
+//* for (x of iterable) {
+//*   code block to be executed
+//* }
+
+console.log("****************")
+for (let key of Object.keys(people)) {
+  console.log(key)
+}
+
+console.log("****************")
+//? people objesindeki tum salary 'leri yazdir
+for (let v of Object.values(people)) {
+  console.log(v.job)
+  // console.log(v["salary"]);
+}
