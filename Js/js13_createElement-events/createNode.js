cc303dd624ee4c48e37539b2c9cb8213e5190b6a
Umit <yildirimsinop>
2023-06-01 20:50:53 +0200

    DOM

--- Js/js13_createElement-events/createNode.js
+++ Js/js13_createElement-events/createNode.js
@@ -1,5 +1,7 @@
 console.log("**** CREATE NODE ****")
 
+//* ------- YENI BIR ELEMENT OLUSTURMA ------
+
 const langInput = document.querySelector("#input")
 
 //? 1- olustur
@@ -20,5 +22,69 @@ const main = document.querySelector("main")
 main.appendChild(newP)
 
 //* item-list section'nın onune yeni elementi ekler.
+//! Element.before()  ve Element.after() appenChild gibi aslında bir elementi DOM Tree'ye baglar.
 const itemListSection = document.querySelector(".item-list")
 itemListSection.before(newP)
+
+//* ------- ID, CLASS GIBI OZELLIKLERI ATAMA ------
+//? 1) Yontem
+newP.id = "paragraf"
+newP.className = "par center"
+newP.name = "deneme"
+
+//? 2) setAttribute()
+newP.setAttribute("id", "new-id")
+newP.setAttribute("class", "new-class")
+newP.setAttribute("name", "new-class")
+newP.setAttribute("type", "button")
+
+//? 3) classList
+newP.classList.add("bg-danger", "border")
+
+console.log(newP.classList.contains("new-class"))
+
+//* classList.contains()
+if (newP.classList.contains("border")) {
+  newP.classList.add("border-success")
+}
+
+//* classList.remove()
+if (newP.classList.contains("new-class")) {
+  newP.classList.remove("new-class")
+}
+
+//* classList.toggle()  (bir class varsa kaldirir yoksa ekler)
+newP.classList.toggle("bg-danger")
+
+//* ------- ID, CLASS GIBI OZELLIKLERINI OKUMA ------
+//? 1) Yontem
+console.log(newP.id)
+console.log(newP.className)
+
+//? 2) getAttribute()
+console.log(newP.getAttribute("type"))
+console.log(newP.getAttribute("class"))
+
+//* ------- YENI BIR ELEMENT OLUSTURMA (INNERHTML)------
+const ul = document.querySelector("ul")
+
+ul.innerHTML += ` 
+   <li>C++</li>
+   <li>java</li>
+   <li>C</li>
+   <li>go</li>`
+
+//* ------- YENI BIR ELEMENT OLUSTURMA (INNERHTML)------
+
+const newDiv = document.createElement("div")
+newDiv.id = "my-div"
+itemListSection.after(newDiv)
+
+newDiv.innerHTML += `
+<h2 class="par center">Languages</h2>
+<ul>
+  <li>C++</li>
+  <li>java</li>
+  <li>C</li>
+  <li>go</li>
+</ul>`
