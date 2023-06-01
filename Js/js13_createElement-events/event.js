4fa6485ca54ac730ce2520ed8370afbea2cf90b4
Umit Yildirim <umity@uia.no>
2023-06-02 00:57:57 +0200

    siste

--- Js/js13_createElement-events/event.js
+++ Js/js13_createElement-events/event.js
@@ -55,10 +55,63 @@ addBtn.addEventListener("click", (e) => {
 
 //? input'un üzerinde iken bir klavye tusuna basilirsa calis
 document.querySelector("#input").addEventListener("keydown", (e) => {
-  //   console.log(e.code)
-  //   console.log(e.keyCode)
+    console.log(e.code)
+    console.log(e.keyCode)
   //? Basilan tus Enter ise
   if (e.code === "Enter") {
     addBtn.click() //? add butonun tiklanma fonksiyonu calistir.
+  }
+})
+
+
+// Önce bir silme butonu oluşturalım
+const deleteBtn = document.createElement("button")
+deleteBtn.innerText = "Delete Last Item"
+deleteBtn.id = "delete-btn"
+document.querySelector(".add-item").appendChild(deleteBtn)
+
+// "Delete Last Item" butonuna tıklanıldığında son eklenen öğeyi silme
+deleteBtn.addEventListener("click", () => {
+  const lastItem = document.querySelector("ul li:last-child")
+  if (lastItem) {
+    lastItem.remove()
+  } else {
+    alert("There are no items to delete!")
+  }
+})
+
+// Input içinde "Delete" tuşuna basıldığında son eklenen öğeyi silme
+document.querySelector("#input").addEventListener("keydown", (e) => {
+  if (e.code === "Delete") {
+    const lastItem = document.querySelector("ul li:last-child")
+    if (lastItem) {
+      lastItem.remove()
+    } else {
+      alert("There are no items to delete!")
+    }
+  }
+})
+
+// Her yeni öğe eklenirken silme butonu ekleyelim
+addBtn.addEventListener("click", (e) => {
+  if (!input.value) {
+    alert("Please enter a comment")
+  } else {
+    const input = document.querySelector("#input")
+    const li = document.createElement("li")
+    ul.appendChild(li)
+    const textLi = document.createTextNode(input.value)
+    li.appendChild(textLi)
+
+    const deleteItemBtn = document.createElement("button")
+    deleteItemBtn.innerText = "Delete Item"
+    li.appendChild(deleteItemBtn)
+    
+    deleteItemBtn.addEventListener("click", () => {
+      li.remove()
+    })
+
+    input.value = ""
+    input.focus()
   }
 })
