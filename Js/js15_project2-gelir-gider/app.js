83130fd22bf8db787efd23ef21ed045890b1a7f3
Umit Yildirim <umity@uia.no>
2023-06-07 21:06:57 +0200

    js

--- Js/js15_project2-gelir-gider/app.js
+++ Js/js15_project2-gelir-gider/app.js
@@ -1,7 +1,65 @@
 //? Selectors
 
+const ekleBtn = document.getElementById ("ekle-btn")
+const gelirInput = document.getElementById ("gelir-input")
+const ekleFormu = document.getElementById ("ekle-formu")
+
+
+//? Sonuc tablosu
+
+const gelirinizTd = document.getElementById ("geliriniz")
+
+//? Harcama formu
+
+const harcamaFormu = document.getElementById("harcama-formu")
+const harcamaAlaniInput = document.getElementById("harcama-alani")
+const tarihInput = document.getElementById("tarih")
+const miktarInput = document.getElementById("miktar")
+
 //? Variables
+
+let gelirler = "0"
+
 
 //?Events
 
+//? Formun submit butonuna basildiginda girer
+
+ekleFormu.addEventListener("submit", (e) => {
+    e.preventDefault()
+gelirler = Number(gelirler) + Number(gelirInput.value) //? string eklemeyi engelledik.
+
+localStorage.setItem("gelirler", gelirler)
+
+//? Input degerini sifirladik
+ekleFormu.reset()
+
+//? Degisiklikleri sonuc tablosuna yazan fonks.
+hesaplaVeGuncelle()
+
+})
+
+//! Sayfa her yuklendikten sonra calisan event
+window.addEventListener("load", () => {
+    gelirler = Number(localStorage.getItem("gelirler"))
+    hesaplaVeGuncelle ()
+})
+
+harcamaFormu.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const yeniHarcama = {id : new Date().getTime(), tarih : tarihInput.value, alan : harcamaAlaniInput.value, miktar : miktarInput.value
+    }
+    harcamaFormu.reset()
+    console.log(yeniHarcama);
+
+})
+
+
 //? Functions
+
+const hesaplaVeGuncelle = () => {
+
+    gelirinizTd.innerText = gelirler
+
+}
