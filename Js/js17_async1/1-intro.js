7905abf3d70ca108e11dc93b5dcd312bbb913140
Umit Yildirim <umity@uia.no>
2023-06-14 20:32:26 +0200

    hel

--- Js/js17_async1/1-intro.js
+++ Js/js17_async1/1-intro.js
@@ -18,29 +18,62 @@
 
 //* Senkron
 //* ------------------------------------------------
-
 // const delay = (waitingTime) => {
-//     const startTime = new Date().getTime ()
-    
-//     while (new Date ().getTime() < startTime + waitingTime) {}
-    
+//   const startTime = new Date().getTime()
+//   while (new Date().getTime() < startTime + waitingTime) {}
 // }
 
-// console.timeEnd ("gecikme");
 // console.log("Hello")
+// // alert("Blocked") //? Blocking
+// console.time("gecikme")
+// // delay(4000) //? blocking code - senkron
+// console.timeEnd("gecikme")
+// console.log("hi")
 
-// // alert("blokked") //? Blocking 
-
-// delay (4000)
-// console.log("hi");
-
-//* ASenkron
+//* Asenkron (setTimeout())
 //* ------------------------------------------------
 
-console.log("start");
+// setTimeout(() => {
+//   console.log("I am fine")
+//   // console.timeEnd("timer")
+// }, 1000)
+
+// setTimeout(() => {
+//   console.log("Whats up?")
+//   // console.timeEnd("timer")
+// }, 1000)
+
+// console.log("start")
+// // console.time("timer")
+
+//* Asenkron (setInterval, clearInterval)
+//*------------------------------------------------
+//? setInterval periyodik zaman araligi oluşturmak icin kullanilabilir.
+//? clearInterval yardımıyla surekli devam interval pasif hale getirilir.
+// let count = 0
+// const sec1 = setInterval(() => {
+//   console.log(++count)
+//   if (count > 9) {
+//     clearInterval(sec1)
+//   }
+// }, 1000)
+
+//! Callback Hell (nested ve birbirine bagli callback'ler)
+//!-----------------------------------------------------
+//* Eger birbirine bagimli asenkron kodlarin yazilmasi gerekirse,nested callback
+//* yapisinin kullanilmasi gerekebilir. Fakat bu iyi bir programlama yaklasimi degildir.
+// !callback hell olarak adlandirilan bu yapinin anlasilmasi ve surdurulebilirligi oldukca zordur.
 
 setTimeout(() => {
-    console.log("Data fetched");
-}, 0)
-
-console.log("start");
+    console.log("john:Hi")
+    setTimeout(() => {
+      console.log("Sarah: Hello")
+      setTimeout(() => {
+        console.log("John: How Are you?")
+        setTimeout(() => {
+          console.log("Sarah:Fine and you?")
+        }, 1000)
+      }, 1000)
+    }, 1000)
+  }, 1000)
+  
