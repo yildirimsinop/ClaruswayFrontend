3fbc0265ce0c826ec94909aadcef2a35ace35fbd
Umit <yildirimsinop>
2023-06-14 20:33:22 +0200

    løsning

--- Js/js17_async1/1-intro.js
+++ Js/js17_async1/1-intro.js
@@ -77,3 +77,11 @@ setTimeout(() => {
     }, 1000)
   }, 1000)
   
+
+  //? COZUMLER:
+//?----------------------------------------------------
+//* 1- XMLHttpRequest (Eski yontem, Ornek: AJAX)
+//? https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+//* 2- Promise,
+//! 3- Fetch API (Promise'in basitlestirilmis hali),
+//! 4- ASYNC-AWAIT (Fetch API'nin makyajlanmis hali)
