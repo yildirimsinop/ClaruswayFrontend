5782c1622f12b09edda97b1f34bd4431ebf19368
Umit <yildirimsinop>
2023-06-14 20:55:16 +0200

    promis

--- Js/js17_async1/2-promise.js
+++ Js/js17_async1/2-promise.js
@@ -31,3 +31,18 @@
 //? Zincirleme olarak kullanilabilirler.
 
 console.log("Promise")
+
+const networkReq = new Promise ((resolve, reject) => {
+    const data = { a: 1, b: 2}
+
+    const success = Math.floor(Math.random()*5) //?0,1,2,3,4
+
+    if (success) {
+        console.log("Data fetched");
+        resolve(data)
+    } else {
+        reject("Ohh no there is network error")
+    }
+})
+
+networkReq.then ( (response) => console.log(response)).catch((err) => document.write(err))
