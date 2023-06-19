63ad2e43e7e46b619409461f8073addb2351d3e9
Umit <yildirimsinop>
2023-06-19 22:02:58 +0200

    semantic

--- Js/projects/005-Crypto-Coin-App/crypto.js
+++ Js/projects/005-Crypto-Coin-App/crypto.js
@@ -6,14 +6,14 @@ const coinList = document.querySelector(".ajax-section .container .coins");
 
 //localStorage
 localStorage.setItem("apiKey", EncryptStringAES("coinranking254889bccdb8bff17fa6e0d89d489d31f7e29af752b833ae"));
-form.addEventListener("submit", (e)=>{
+form.addEventListener("submit", (e) => {
     e.preventDefault();
     getCoinDataFromApi();
     //form.reset == e.target.reset
     e.target.reset();
 });
 
-const getCoinDataFromApi = async() =>{
+const getCoinDataFromApi = async () => {
     //alert("Get Coin Data!!");
     const apiKey = DecryptStringAES(localStorage.getItem("apiKey"));
     //console.log(apiKey);
@@ -21,44 +21,69 @@ const getCoinDataFromApi = async() =>{
     const url = `https://api.coinranking.com/v2/coins?search=${input.value}&limit=1`;
     const options = {
         headers: {
-          'x-access-token': apiKey,
+            'x-access-token': apiKey,
         },
     };
-      //fetch vs. axios
+    //fetch vs. axios
     //   const response = await fetch(url, options)
     //   .then((response) => response.json())
     //   .then((result) => console.log(result.data.coins[0]));
-    const response = await axios(url, options);
-    //console.log(response.data.data.coins[0]);
-    //obj. dest.
-    const { price, name, change, iconUrl, symbol } = response.data.data.coins[0];
-    //console.log(iconUrl);
+    try{
+        const response = await axios(url, options);
+        //console.log(response.data.data.coins[0]);
+        //obj. dest.
+        const { price, name, change, iconUrl, symbol } = response.data.data.coins[0];
+        //console.log(iconUrl);
+    
+        //Coin Control!!!
+        const coinNameSpans = coinList.querySelectorAll("h2 span");
+        //filter vs. map(array)
+        if (coinNameSpans.length > 0) {
+            const filteredArray = [...coinNameSpans].filter(span => span.innerText == name);
+            console.log(filteredArray);
+            if (filteredArray.length > 0) {
+                msgSpan.innerText = `You already know the data for ${name}, Please search for another coin 😉`;
+                setTimeout(() => {
+                    msgSpan.innerText = "";
+                }, 3000);
+                return;
+            }
+        }
 
-    const createdLi = document.createElement("li");
-    createdLi.classList.add("coin");
-    createdLi.innerHTML = `
-        <h2 class="coin-name" data-name=${name}>
-            <span>${name}</span>
-            <sup>${symbol}</sup>
-        </h2>
-        <div class="coin-temp">$${Number(price).toFixed(6)}</div>
-        <figure>
-            <img class="coin-icon" src=${iconUrl}>                
-            <figcaption style='color:${change < 0 ? "red" : "green"}'>
-                <span><i class="fa-solid fa-chart-line"></i></span>
-                <span>${change}%</span>
-            </figcaption>
-        </figure>
-        <span class="remove-icon">
-            <i class="fas fa-window-close" style="color:red"></i>
-        </span>`;
+        //continue xxxx return
+        const createdLi = document.createElement("li");
+        createdLi.classList.add("coin");
+        createdLi.innerHTML = `
+            <h2 class="coin-name" data-name=${name}>
+                <span>${name}</span>
+                <sup>${symbol}</sup>
+            </h2>
+            <div class="coin-temp">$${Number(price).toFixed(6)}</div>
+            <figure>
+                <img class="coin-icon" src=${iconUrl}>                
+                <figcaption style='color:${change < 0 ? "red" : "green"}'>
+                    <span><i class="fa-solid fa-chart-line"></i></span>
+                    <span>${change}%</span>
+                </figcaption>
+            </figure>
+            <span class="remove-icon">
+                <i class="fas fa-window-close" style="color:red"></i>
+            </span>`;
         //append vs. prepend
         //append vs appendChild
         coinList.prepend(createdLi);
         //remove func.
-        createdLi.querySelector(".remove-icon").addEventListener("click", ()=>{
+        createdLi.querySelector(".remove-icon").addEventListener("click", () => {
             createdLi.remove();
         });
-        //
+    }
+   catch(error){
+    //error logging
+    //postErrorLog("crypto.js", "getCoinDataFromApi", new Date(), error...);
+    msgSpan.innerText = `Coin not found!`;
+    setTimeout(() => {
+        msgSpan.innerText = "";
+    }, 3000);
+   }
 
 }
