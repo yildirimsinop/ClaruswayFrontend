8c4d17095c29755e1fae90a283b1d07d5f15f4c1
Umit <yildirimsinop>
2023-06-20 19:56:32 +0200

    book

--- Js/js20_oop/1-classesES6.js
+++ Js/js20_oop/1-classesES6.js
@@ -16,3 +16,46 @@ const book1 = {
     return `${this.title} was written by ${this.author} in ${this.year}`
   },
 }
+
+console.log(book1.getSummary())
+
+const book2 = {
+  title: "The Karamazov Brothers",
+  author: "Dostoevski",
+  year: 1886,
+  getSummary: function () {
+    return `${this.title} was written by ${this.author} in ${this.year}`
+  },
+}
+console.log(book2.getSummary())
+
+//? Classes'lar, object (nesne) olusturmak icin kullanilan sablonlardir.
+//? JS'de Class'lar prototipler uzerine insa edilmistir. Ancak, syntax
+//? ES5'den farklidir. Aslinda, class keyword'u ilk olarak ES6 da
+//? kullanilmistir ancak bu sadece bir syntactical sugar'dir.
+//? JavaScript, class-tabanli bir dil degil, prototype-tabanli bir dildir.
+//? yaygin kullanim class-tabanli oldugu icin syntax'ini O'na benzetmistir.
+
+//? Bir parent class'in degisken ve fonksiyonelliği extends
+//? keyword'u ile child class'a gecmektedir.(INHERITANCE)
+
+console.log(" *****  CLASSES ******")
+
+class Book {
+  constructor(title, author, year) {
+    this.title = title
+    this.author = author
+    this.year = year
+  }
+
+  getSummary() {
+    return `${this.title} was written by ${this.author} in ${this.year}`
+  }
+}
+//! Book kalibinda yeni bir ornek (instance) olusturduk.
+const book3 = new Book("Stupid Reseaches", "XYZ", 2022)
+const book4 = new Book("Dummy Reseaches", "ABC", 2023)
+
+console.log(book3)
+console.log(book3.getSummary())
+console.log(book4.getSummary())
