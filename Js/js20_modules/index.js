cb05c961fbbfe26defb4417072e045f16eaa2875
Umit Yildirim <umity@uia.no>
2023-06-20 22:09:52 +0200

    art

--- Js/js20_modules/index.js
+++ Js/js20_modules/index.js
@@ -4,3 +4,18 @@ console.log(window);
 
 //? Module kullaniminda this objesi window nesnesini gostermez.
 console.log(this); //? undefined
+
+//? named export (import)
+import {CALISMA_SAATI, maas} from "./mymodule.js" 
+import {arttir, azalt} from "./mymodule.js" 
+
+console.log(CALISMA_SAATI);
+console.log(maas);
+
+console.log(arttir(5));
+console.log(azalt(15));
+
+//? export default
+import selamVer from "./mymodule.js";
+
+selamVer()
--- Js/js20_modules/mymodule.js
+++ Js/js20_modules/mymodule.js
@@ -1,1 +1,21 @@
 console.log("My Module Loaded")
+
+//? Named Export
+export const CALISMA_SAATI = 40
+
+export let maas = 20000
+
+let sayac = 0
+export const arttir = (miktar) => {
+sayac += miktar
+return sayac
+}
+export const azalt = (miktar) => {
+sayac -= miktar
+return sayac
+}
+
+//! Export Default (Her bir modul icin sadece bir tane default export edilebilir.)
+export default function selamVer() {
+    alert("Merhaba FS14")
+}
