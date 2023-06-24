d118060a6d6b2f75ad4358056a3de1c127dc5af4
Umit <yildirimsinop>
2023-06-24 12:12:11 +0200

    component

--- React/rc02_comp-jsx/src/App.js
+++ React/rc02_comp-jsx/src/App.js
@@ -1,9 +1,14 @@
+import { Footer } from "./Footer"
 import Header from "./Header"
+import "./App.css"
 
 function App() {
+  console.log(App)
   return (
     <div>
       <Header/>
+      <Content/>
+      <Footer/>
     </div>
   )
 }
