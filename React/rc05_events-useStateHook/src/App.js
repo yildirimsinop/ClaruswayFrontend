366df79da10c9744570974501e97c8db3a8fd068
Umit Yildirim <umity@uia.no>
2023-07-09 22:32:14 +0200

    evnet

---
+++ React/rc05_events-useStateHook/src/App.js
@@ -0,0 +1,15 @@
+// // import Events from "./1-events/Events"
+// // import Counter from "./2-classComponent/Counter"
+// import UseStateCounter from "./3-useState/UseStateCounter"
+// import UseStateObject from "./3-useState/UseStateObject"
+
+import Events from "./1-events/Events";
+
+function App() {
+  return (
+    <div>
+      <Events />
+    </div>
+  );
+}
+export default App;
