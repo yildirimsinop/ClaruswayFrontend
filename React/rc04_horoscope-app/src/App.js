181128d555655d6c8c7fc91933926ee0d423e28b
Umit <yildirimsinop>
2023-07-09 17:23:32 +0200

    change

--- React/rc04_horoscope-app/src/App.js
+++ React/rc04_horoscope-app/src/App.js
@@ -1,10 +1,12 @@
 import "./App.scss";
 import Header from "./components/header/Header";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Navbar />
     </div>
   );
 }
