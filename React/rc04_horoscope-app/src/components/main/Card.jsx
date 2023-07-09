941671d886d25a6f053588806c652264c5edb48f
Umit <yildirimsinop>
2023-07-09 21:13:38 +0200

    card

--- React/rc04_horoscope-app/src/components/main/Card.jsx
+++ React/rc04_horoscope-app/src/components/main/Card.jsx
@@ -1,23 +1,19 @@
-const Card = ({title, date, image, desc}) => {
-  // console.log("nedir", props)
-  // const {title, date, mage, desc} = props;
-  return(
+const Card = ({ title, date, image, desc }) => {
+  //   console.log("ne geliyor", props);
+  //   const { title, date, image, desc } = props;
+  return (
     <div className="cards">
       <div className="title">
-        <h1>props.title</h1>
-
+        <h1>{title}</h1>
       </div>
       <div className="date">
-        <h2>date</h2>
+        <h2>{date}</h2>
       </div>
-      <img src="" alt="" />
+      <img src={image} alt="" />
       <div className="card-over">
-        <p>dexcription</p>
-
+        <p>{desc}</p>
       </div>
-
     </div>
-  )
-}
-
+  );
+};
 export default Card;
