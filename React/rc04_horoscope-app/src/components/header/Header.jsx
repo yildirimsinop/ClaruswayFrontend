2fe19506ceed355f1bce42c9e407e8d9be9e8d65
Umit Yildirim <umity@uia.no>
2023-07-09 18:47:19 +0200

    navbar

--- React/rc04_horoscope-app/src/components/header/Header.jsx
+++ React/rc04_horoscope-app/src/components/header/Header.jsx
@@ -1,11 +1,10 @@
 import "./Header.scss";
-
 const Header = () => {
-    return(
-        <div className="header">
-            <span className="text-shadow">Who Are You?</span>
-        </div>
-    )
-}
+  return (
+    <div className="header">
+      <span className="text-shadow">Who Are You?</span>
+    </div>
+  );
+};
 
 export default Header;
