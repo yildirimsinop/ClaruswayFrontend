ca4a1927211b56ea555e07c4f5745975891a1224
Umit Yildirim <umity@uia.no>
2023-07-09 17:23:32 +0200

    change

---
+++ React/rc04_horoscope-app/src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import logo from "../../helpers/logo.png";
+const Navbar = () => {
+    return(
+        <div className="navbar">
+            <div className="logo">
+                <img src="{logo}" alt="horoscope" />
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
