769352acb35ee8503ffbebc0914e7e816945b4c1
Umit Yildirim <umity@uia.no>
2023-07-09 21:13:38 +0200

    card

--- React/rc04_horoscope-app/src/components/main/Main.jsx
+++ React/rc04_horoscope-app/src/components/main/Main.jsx
@@ -1,13 +1,14 @@
-import {data} from "../../helpers/data";
+import { data } from "../../helpers/data";
 import Card from "./Card";
+import "./Main.scss";
 const Main = () => {
-  return(
+  console.log(data);
+  return (
     <div className="card-container">
-      {data.map((item)=> (
-      <Card key= {item.id} {...item}/>
+      {data.map((item) => (
+        <Card key={item.id} {...item} />
       ))}
     </div>
-  )
-}
-
+  );
+};
 export default Main;
