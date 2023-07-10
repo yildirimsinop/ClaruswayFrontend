5dd2ccd6372a7dc452866bf078e8bd18cf705bef
Umit Yildirim <umity@uia.no>
2023-07-10 21:04:59 +0200

    nba

--- React/rc07_nba-legends-app/inclass/src/App.js
+++ React/rc07_nba-legends-app/inclass/src/App.js
@@ -1,7 +1,13 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import ContainerCard from "./components/ContainerCard";
 
 function App() {
   return (
-    <div>Hello React</div>
+    <Container className="text-center mt-5">
+      <Header />
+      <ContainerCard />
+    </Container>
   );
 }
 
