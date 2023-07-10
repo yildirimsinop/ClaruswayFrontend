4299f6ab2a96d253780f78a50755d482b06a0595
Umit Yildirim <umity@uia.no>
2023-07-10 03:12:57 +0200

    change

--- React/rc05_events-useStateHook/src/App.js
+++ React/rc05_events-useStateHook/src/App.js
@@ -3,12 +3,16 @@
 // import UseStateCounter from "./3-useState/UseStateCounter"
 // import UseStateObject from "./3-useState/UseStateObject"
 
-import Events from "./1-events/Events";
+// import Events from "./1-events/Events";
+// import Counter from "./2-classComponent/Counter";
+import UseStateCounter from "./3-useState/UseStateCounter";
 
 function App() {
   return (
     <div>
-      <Events />
+      {/* <Events />
+      <Counter/> */}
+      <UseStateCounter />
     </div>
   );
 }
