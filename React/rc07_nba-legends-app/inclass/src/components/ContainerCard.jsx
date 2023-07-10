65620470ea4ba332f7667d911f8d958b15ce88f1
Umit <yildirimsinop>
2023-07-10 22:37:02 +0200

    mark

--- React/rc07_nba-legends-app/inclass/src/components/ContainerCard.jsx
+++ React/rc07_nba-legends-app/inclass/src/components/ContainerCard.jsx
@@ -1,18 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { data } from "../helpers/data";
+import PlayerCard from "./PlayerCard";
 
 const ContainerCard = () => {
+  const [search, setSearch] = useState("");
+  console.log(data);
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setSearch(e.target.value); //! setter mtodları asenkron olarak çalışır.
+    // console.log(search)
+  };
+  console.log(search);
+
+  const filteredData = data.filter((item) =>
+    item.name.toLowerCase().includes(search.trim().toLowerCase())
+  );
+
+  console.log("filter", filteredData);
+
   return (
     <>
-      <Form.Control type="search" placeholder="Search Player..." />
-      <Container className="p-3 rounded-4 card-container">
+      <Form.Control
+        type="search"
+        placeholder="Search Player..."
+        onChange={handleChange}
+      />
+      <Container className="p-3 rounded-4 card-container my-3">
         <Row xs={2} md={4} lg={6} className="justify-content-center g-3">
           <Col>1 of 2</Col>
-          <Col>2 of 2</Col>
-          <Col>3 of 3</Col>
+          {/* {filteredData.map((player, i) => (
+            <PlayerCard
+              key={i}
+              player={player}
+              img={player.img}
+              name={player.name}
+              statistics={player.statistics}
+              {...player}
+            />
+          ))} */}
+          {filteredData.map((player, i) => (
+            <PlayerCard key={i} {...player} />
+          ))}
         </Row>
       </Container>
     </>
