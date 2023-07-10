b7c0e616af4676856fec2f71e26eb8b6e8391c76
Umit Yildirim <umity@uia.no>
2023-07-10 03:36:36 +0200

    state

--- React/rc05_events-useStateHook/src/App.js
+++ React/rc05_events-useStateHook/src/App.js
@@ -1,5 +1,3 @@
-// // import Events from "./1-events/Events"
-// // import Counter from "./2-classComponent/Counter"
 // import UseStateCounter from "./3-useState/UseStateCounter"
 // import UseStateObject from "./3-useState/UseStateObject"
 
