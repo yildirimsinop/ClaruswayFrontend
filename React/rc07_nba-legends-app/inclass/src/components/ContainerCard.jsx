121dfc3ca88f3fe00488fcaecd0f1910f3e3f06d
Umit <yildirimsinop>
2023-07-10 22:41:08 +0200

    change

--- React/rc07_nba-legends-app/inclass/src/components/ContainerCard.jsx
+++ React/rc07_nba-legends-app/inclass/src/components/ContainerCard.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+// import Col from "react-bootstrap/Col";
 import { data } from "../helpers/data";
 import PlayerCard from "./PlayerCard";
 
@@ -30,18 +30,29 @@ const ContainerCard = () => {
         onChange={handleChange}
       />
       <Container className="p-3 rounded-4 card-container my-3">
-        <Row xs={2} md={4} lg={6} className="justify-content-center g-3">
-          <Col>1 of 2</Col>
+        <Row xs={1} sm={2} md={3} lg={4} className="justify-content-center g-3">
           {/* {filteredData.map((player, i) => (
             <PlayerCard
               key={i}
               player={player}
+              //{player}
+              // const {img,name,statistics} = player;
+              //player.img
               img={player.img}
               name={player.name}
               statistics={player.statistics}
-              {...player}
+              // {name,img,statistics}
+              {...player} // objenin içerisndeki tüüm veriler lazımsa 
+              // {name,img,statistics}
             />
           ))} */}
+          {/* {data
+            .filter(item =>
+              item.name.toLowerCase().includes(search.trim().toLowerCase())
+            )
+            .map((player, i) => (
+              <PlayerCard key={i} {...player} />
+            ))} */}
           {filteredData.map((player, i) => (
             <PlayerCard key={i} {...player} />
           ))}
