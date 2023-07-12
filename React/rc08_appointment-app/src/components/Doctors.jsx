2b5022eb1eb28c0bba9d88d8b50f01c9934e8b6e
Umit <yildirimsinop>
2023-07-12 20:53:55 +0200

    submit

--- React/rc08_appointment-app/src/components/Doctors.jsx
+++ React/rc08_appointment-app/src/components/Doctors.jsx
@@ -2,23 +2,40 @@ import Container from "react-bootstrap/Container";
 import { doctorData } from "../helper/data";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import AddModal from "./AddModal";
+import { useState } from "react";
 
 const Doctors = () => {
-  console.log(doctorData);
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleImgClick = () => {
+    handleShow();
+  };
+
   return (
     <Container className="p-2">
       <h3 className="display-6 mb-3" style={{ color: "rgb(166, 18, 189)" }}>
         Our Doctors
       </h3>
-      <Row>
+      <Row className="justify-content-center">
         {doctorData.map(({ id, img, dep, name }) => (
-          <Col xs={12} sm={6} md={4} lg={3}>
-            <img src={img} alt={name} className="img-thumbnail doctor-img" />
+          <Col xs={12} sm={6} md={4} lg={3} key={id}>
+            <img
+              src={img}
+              alt={name}
+              className="img-thumbnail doctor-img"
+              onClick={handleImgClick}
+            />
             <h5>{name}</h5>
             <h6>{dep}</h6>
           </Col>
         ))}
       </Row>
+
+      <AddModal show={show} handleClose={handleClose} />
     </Container>
   );
 };
