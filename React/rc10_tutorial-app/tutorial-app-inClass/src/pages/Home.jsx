ea64a4b56ede7538e77d37ab21e6883494d353e3
Umit Yildirim <umity@uia.no>
2023-07-17 20:04:32 +0200

    getTutorials

--- React/rc10_tutorial-app/tutorial-app-inClass/src/pages/Home.jsx
+++ React/rc10_tutorial-app/tutorial-app-inClass/src/pages/Home.jsx
@@ -1,7 +1,25 @@
+import { useState, useEffect } from "react";
 import AddTutorial from "../components/AddTutorial";
 import TutorialList from "../components/TutorialList";
+import axios from "axios";
 
 const Home = () => {
+  const [tutorials, setTutorials] = useState([]);
+
+  const BADE_URL = "https://tutorial-api.fullstack.clarusway.com/tutorials/";
+
+  const getTutorials = async () => {
+    const data = await axios(BADE_URL);
+    console.log(data.data);
+    setTutorials(data.data);
+  };
+
+  console.log(tutorials);
+
+  useEffect(() => {
+    getTutorials();
+  });
+
   return (
     <>
       <AddTutorial />
