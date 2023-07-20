bacfdd11d860b0ca9ed8f699f6a98a54a3a083f3
Umit Yildirim <umity@uia.no>
2023-07-20 20:06:27 +0200

    rooter

--- React/rc11-12_router-example/rc11-12_router-example-inClass/src/pages/PersonDetail.jsx
+++ React/rc11-12_router-example/rc11-12_router-example-inClass/src/pages/PersonDetail.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
+import NotFound from "./NotFound";
+import spinner from "../img/Spinner-2.gif";
+
 const PersonDetail = () => {
   //! navigate ile gonderilen state'i yakalamak icin useLocation Hook'u kullanilabilir.
   //! Bu durumda veri, state ile geldigi icin yeniden fetch yapilmasina gerek kalmaz
@@ -9,18 +12,34 @@ const PersonDetail = () => {
   // console.log(person);
   //! Linkteki parametreyi almak icin useParams Hook'u kullanilabilir.
   let { id } = useParams();
-  console.log({ id });
+  // console.log({ id });
   const [person, setPerson] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const getPerson = () => {
     axios(`https://reqres.in/api/users/${id}`)
       .then((res) => setPerson(res.data.data))
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        setError(true);
+        console.log(err);
+      })
+      .finally(() => setLoading(false));
   };
 
   useEffect(() => {
     getPerson();
   }, []);
+
+  if (error) {
+    return <NotFound />;
+  } else if (loading) {
+    return (
+      <div className="text-center mt-4">
+        <img src={spinner} alt="spinner" />
+      </div>
+    );
+  }
 
   return (
     <div className="container text-center">
