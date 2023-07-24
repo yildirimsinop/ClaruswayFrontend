e7518903a63edba10eafee4d6410e9ace8fa3c7c
Umit Yildirim <umity@uia.no>
2023-07-24 20:04:53 +0200

    change

--- React/rc14_styled-comps/styled-comp-intro/src/App.js
+++ React/rc14_styled-comps/styled-comp-intro/src/App.js
@@ -1,7 +1,15 @@
+import { Button, DarkButton } from "./components/Buton.styled";
+import Flex from "./components/Flex";
+
 const App = () => {
   return (
     <>
-      <h1>Styled Components</h1>
+      <Flex bg="purple">
+        <h1>Styled Components</h1>
+        <Button>Tikla</Button>
+        <DarkButton>Send</DarkButton>
+      </Flex>
+      <Flex bg="tomato"></Flex>
     </>
   );
 };
