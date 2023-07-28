5f00f590d2fd31914d1ff44133a6156a64c4aff8
Umit Yildirim <umity@uia.no>
2023-07-28 13:43:42 +0200

    default

--- React/rc15_clarus-library/inclass/src/components/header/Header.jsx
+++ React/rc15_clarus-library/inclass/src/components/header/Header.jsx
@@ -7,15 +7,42 @@ import {
   SearchInput,
   SelectBox,
 } from "./Header.style";
+import { useBooksContext } from "../../context/BooksContext";
 
 const Header = () => {
+  const printType = ["all", "books", "magazines"];
+  const { searchInfo, setSearchInfo, getData } = useBooksContext();
+  const handleChange = (e) => {
+    console.log(e.target);
+    console.log(e.name);
+    setSearchInfo({ ...searchInfo, [e.target.name]: e.target.value }); // inputun name attr ile statede ki key isimleri ayni olmak zorunda.
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getData();
+  };
+
+  console.log(searchInfo);
+
   return (
     <HeaderContainer>
       <HeaderTitle>BOOKS OR MAGAZINES</HeaderTitle>
       <HeaderForm>
-        <SearchInput type="search" placeholder="Search..." />
-        <SelectBox>
-          <option>ALL</option>
+        <SearchInput
+          type="search"
+          placeholder="Search..."
+          name="query"
+          value={searchInfo.query}
+          onChange={handleChange}
+          required
+        />
+        <SelectBox value={searchInfo.selectType} onChange={handleChange}>
+          {printType.map((item) => (
+            <option key={item} value={item}>
+              {item}
+            </option>
+          ))}
         </SelectBox>
         <HeaderButton>Search</HeaderButton>
       </HeaderForm>
