4205449e937e3cbebc85ff1049a1830111054951
Umit Yildirim <umity@uia.no>
2023-07-30 18:41:36 +0200

    tailw

--- React/rc17-18_movie-app/tailwindcss-intro/src/App.js
+++ React/rc17-18_movie-app/tailwindcss-intro/src/App.js
@@ -1,8 +1,10 @@
 function App() {
   return (
-    <div class>
-      <h1>TailwindCss Kurulum</h1>
+    <div className="bg-fuchsia-300 h-screen p-[50px]">
+      <h1 className="text-8xl bg-[#ededed]">TailwindCss Kurulum</h1>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
+      <div className="flex flex-col justify-between "></div>
     </div>
   );
 }
