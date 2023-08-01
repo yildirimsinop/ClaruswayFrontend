b18d40fd7e2d44f90b6811fb415b9b2e188d6ede
Umit Yildirim <umity@uia.no>
2023-08-01 20:54:31 +0200

    movie

--- React/rc17-18_movie-app/movie-app-inclass/src/pages/Main.jsx
+++ React/rc17-18_movie-app/movie-app-inclass/src/pages/Main.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
+import { MovieContext } from "../context/MovieContext";
 
 const Main = () => {
-  return <div>Main</div>;
+  const { movies } = useContext(MovieContext);
+  console.log(movies);
+  return (
+    <>
+      <div className="flex justify-center flex-wrap">
+        {movies.map((movie) => console.log(movie))}
+      </div>
+    </>
+  );
 };
 
 export default Main;
