e8ebbf095b16686c31c386ff2db15ae63effd108
Umit Yildirim <umity@uia.no>
2023-08-01 22:20:15 +0200

    swich

--- React/rc17-18_movie-app/movie-app-inclass/src/components/MovieCard.jsx
+++ React/rc17-18_movie-app/movie-app-inclass/src/components/MovieCard.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
 const defaultImage =
   "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
 
 const MovieCard = ({ poster_path, title, overview, vote_average, id }) => {
+  const { currentUser } = useContext(AuthContext);
   let navigate = useNavigate();
+
+  const getVoteClass = (vote) => {
+    if (vote >= 8) {
+      return "green";
+    } else if (vote >= 6) {
+      return "orange";
+    } else {
+      return "red";
+    }
+  };
   return (
     <div
       className="movie"
@@ -20,7 +32,11 @@ const MovieCard = ({ poster_path, title, overview, vote_average, id }) => {
       />
       <div className="flex align-baseline justify-between p-1 text-white">
         <h5>{title}</h5>
-        <span>{vote_average.toFixed(1)}</span>
+        {currentUser && (
+          <span className={`tag ${getVoteClass(vote_average)}`}>
+            {vote_average.toFixed(1)}
+          </span>
+        )}
       </div>
       <div className="movie-over">
         <h2>Overview</h2>
