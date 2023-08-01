9f59a3fdb42c1bf0e874a367780a24a3c1142a49
Umit Yildirim <umity@uia.no>
2023-08-01 20:32:58 +0200

    signInWithPopup

--- React/rc17-18_movie-app/movie-app-inclass/src/pages/Register.jsx
+++ React/rc17-18_movie-app/movie-app-inclass/src/pages/Register.jsx
@@ -7,7 +7,7 @@ const Register = () => {
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const { createUser } = useContext(AuthContext);
+  const { createUser, signUpProvider } = useContext(AuthContext);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -76,6 +76,7 @@ const Register = () => {
           <button
             type="button"
             className="btn-danger flex justify-between items-center"
+            onClick={() => signUpProvider()}
           >
             Continue with Google
             <GoogleIcon color="currentColor" />
