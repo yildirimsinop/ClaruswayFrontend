87561a91edcab0bd30e50f82f6702272a1376595
Umit Yildirim <umity@uia.no>
2023-08-01 21:48:19 +0200

    main

--- React/rc17-18_movie-app/movie-app-inclass/src/components/MovieCard.jsx
+++ React/rc17-18_movie-app/movie-app-inclass/src/components/MovieCard.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
 const defaultImage =
   "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
 
-const MovieCard = ({ poster_path, title, overview, vote_average }) => {
+const MovieCard = ({ poster_path, title, overview, vote_average, id }) => {
+  let navigate = useNavigate();
   return (
-    <div className="movie" id="container">
+    <div
+      className="movie"
+      id="container"
+      onClick={() => navigate("/details/" + id)}
+    >
       <img
         loading="lazy"
         src={poster_path ? IMG_API + poster_path : defaultImage}
@@ -14,6 +20,7 @@ const MovieCard = ({ poster_path, title, overview, vote_average }) => {
       />
       <div className="flex align-baseline justify-between p-1 text-white">
         <h5>{title}</h5>
+        <span>{vote_average.toFixed(1)}</span>
       </div>
       <div className="movie-over">
         <h2>Overview</h2>
