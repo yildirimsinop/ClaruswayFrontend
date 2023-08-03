ca02a15bd0240deaaa48f0700217cb783a5691a1
Umit Yildirim <umity@uia.no>
2023-08-03 20:33:11 +0200

    typograpy

--- React/rc19_mui-intro/mui-intro-vite/src/components/Typo.jsx
+++ React/rc19_mui-intro/mui-intro-vite/src/components/Typo.jsx
@@ -4,7 +4,21 @@ import Typography from "@mui/material/Typography"; //? Performans acisindan daha
 const Typo = () => {
   return (
     <div>
-      <Typography>MUI Typo</Typography>
+      <Typography variant="h3" component="h1" color="lightcoral" mt={4}>
+        MUI Typo
+      </Typography>
+      <Typography variant="subtitle2" color="palegreen">
+        MUI Typo
+      </Typography>
+      <Typography variant="button">Button TEXT</Typography>
+
+      <Typography
+        variant="body1"
+        sx={{ color: "blue", backgroundColor: "palevioletred", mt: "1rem" }}
+        align="center"
+      >
+        Button TEXT
+      </Typography>
     </div>
   );
 };
