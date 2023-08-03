ff2901839fdb5f26dc8144686fb314843b35bbe5
Umit Yildirim <umity@uia.no>
2023-08-03 20:03:10 +0200

    typo

--- React/rc19_mui-intro/mui-intro-vite/src/App.jsx
+++ React/rc19_mui-intro/mui-intro-vite/src/App.jsx
@@ -1,7 +1,9 @@
+import Typo from "./components/Typo";
+
 function App() {
   return (
     <>
-      <h1>MUI</h1>
+      <Typo />
     </>
   );
 }
---
+++ React/rc19_mui-intro/mui-intro-vite/src/components/Typo.jsx
@@ -0,0 +1,12 @@
+// import { Typography } from "@mui/material";
+import Typography from "@mui/material/Typography"; //? Performans acisindan daha mantikli
+
+const Typo = () => {
+  return (
+    <div>
+      <Typography>MUI Typo</Typography>
+    </div>
+  );
+};
+
+export default Typo;
