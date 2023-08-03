e9e1b018814e0a7c20d3e63cb4dbe85c5aa3a954
Umit Yildirim <umity@uia.no>
2023-08-03 20:41:21 +0200

    sm

--- React/rc19_mui-intro/mui-intro-vite/src/components/Typo.jsx
+++ React/rc19_mui-intro/mui-intro-vite/src/components/Typo.jsx
@@ -1,25 +1,36 @@
 // import { Typography } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import Typography from "@mui/material/Typography"; //? Performans acisindan daha mantikli
 
 const Typo = () => {
   return (
-    <div>
-      <Typography variant="h3" component="h1" color="lightcoral" mt={4}>
-        MUI Typo
-      </Typography>
-      <Typography variant="subtitle2" color="palegreen">
-        MUI Typo
-      </Typography>
-      <Typography variant="button">Button TEXT</Typography>
-
-      <Typography
-        variant="body1"
-        sx={{ color: "blue", backgroundColor: "palevioletred", mt: "1rem" }}
-        align="center"
+    <Container maxWidth="sm">
+      <Box
+        sx={{
+          padding: "1rem",
+          display: "flex",
+          flexDirection: "column",
+          gap: "1rem",
+          border: "1px solid tomato",
+        }}
       >
-        Button TEXT
-      </Typography>
-    </div>
+        <Typography variant="h3" component="h1" color="lightcoral" mt={4}>
+          MUI Typo
+        </Typography>
+        <Typography variant="subtitle2" color="palegreen">
+          MUI Typo
+        </Typography>
+        <Typography variant="button">Button TEXT</Typography>
+
+        <Typography
+          variant="body1"
+          sx={{ color: "blue", backgroundColor: "palevioletred", mt: "1rem" }}
+          align="center"
+        >
+          Button TEXT
+        </Typography>
+      </Box>
+    </Container>
   );
 };
 
