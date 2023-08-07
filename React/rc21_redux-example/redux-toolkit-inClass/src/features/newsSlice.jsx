2612c5af435483c9d3566eeb7a703250d3255588
Umit Yildirim <umity@uia.no>
2023-08-07 21:32:12 +0200

    get

---
+++ React/rc21_redux-example/redux-toolkit-inClass/src/features/newsSlice.jsx
@@ -0,0 +1,31 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  news: [],
+};
+
+const getNews = createAsyncThunk(
+  "getNewsFunc", //? action type name
+
+  async () => {
+    const API_KEY = "1a1a999e0d7240a6bd2dead87bcca78e";
+    const url = `https://newsapi.org/v2/top-headlines?country=tr&apiKey=${API_KEY}`;
+    const { data } = await axios(url);
+    console.log(data);
+  }
+);
+
+const newsSlice = createSlice({
+  name: "news",
+  initialState,
+  reducers: {
+    clearNews: (state) => {
+      state.news = [];
+    },
+  },
+});
+
+export const {} = newsSlice.actions;
+
+export default newsSlice.reducer;
