27c6dcea9a4646f0e90f3657749ab24f4e4a8390
Umit Yildirim <umity@uia.no>
2023-08-07 20:55:21 +0200

    user

--- React/rc21_redux-example/redux-toolkit-inClass/src/components/Navbar.jsx
+++ React/rc21_redux-example/redux-toolkit-inClass/src/components/Navbar.jsx
@@ -5,9 +5,18 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { clearUser } from "../features/authSlice";
 
 export default function Navbar() {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
+  const handleLogout = () => {
+    dispatch(clearUser());
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" color="secondary">
@@ -21,7 +30,12 @@ export default function Navbar() {
             Clarusway News
           </Typography>
 
-          <Button color="inherit">Login</Button>
+          {user?.email && (
+            <Button color="inherit" onClick={handleLogout}>
+              Logout
+            </Button>
+          )}
+          {!user?.email && <Button color="inherit">Login</Button>}
         </Toolbar>
       </AppBar>
     </Box>
