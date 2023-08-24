af4a28c1edefd7190fa951948e081d54261aad42
Umit Yildirim <umity@uia.no>
2023-08-24 20:39:54 +0200

    js

--- typescript/ClassNotes/S01/out/App.js
+++ typescript/ClassNotes/S01/out/App.js
@@ -1,5 +1,24 @@
-console.log('Hello World');
-let x = 3;
-let y = false;
-//z sonradan kullanılacak
-let z;
+// console.log('Hello World');
+// console.log('Hello World');
+// const button = document.getElementById('submit')! as HTMLButtonElement;
+// const a = document.getElementById('input1')! as HTMLInputElement;
+// const b = document.getElementById('input2')! as HTMLInputElement;
+// const result = document.getElementById('result');
+// function add(a: number, b: number) {
+//     return a + b;
+// }
+// button.addEventListener('click', function () {
+//     result.innerText = `${add(+a.value, +b.value)}`;
+// });
+// //? Type Annotation
+// //? Type Annotation
+// let x : number= 5
+// let y = "Hello World"
+// y = x
+// let z;
+// z = false
+// z= 5
+// z= "Hello World"
+//? Data types
+//? Array
+let num = [1, "string"];
--- typescript/ClassNotes/S01/src/App.ts
+++ typescript/ClassNotes/S01/src/App.ts
@@ -1,9 +1,35 @@
-console.log('Hello World');
+// console.log('Hello World');
 
-let x:number = 3;
+// console.log('Hello World');
+// const button = document.getElementById('submit')! as HTMLButtonElement;
+// const a = document.getElementById('input1')! as HTMLInputElement;
+// const b = document.getElementById('input2')! as HTMLInputElement;
+// const result = document.getElementById('result');
 
-let y = false
+// function add(a: number, b: number) {
+//     return a + b;
+// }
+// button.addEventListener('click', function () {
+//     result.innerText = `${add(+a.value, +b.value)}`;
+// });
 
 
-//z sonradan kullanılacak
-let z:any;
+// //? Type Annotation
+// //? Type Annotation
+
+// let x : number= 5
+
+// let y = "Hello World"
+
+// y = x
+
+// let z;
+
+// z = false
+// z= 5
+// z= "Hello World"
+
+//? Data types
+//? Array
+
+let num : any[] = [1, "string"]
