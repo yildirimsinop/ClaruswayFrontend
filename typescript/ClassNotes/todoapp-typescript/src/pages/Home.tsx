7081e166743437675c96c0adc2f5d3aca36de687
Umit Yildirim <umity@uia.no>
2023-08-28 20:24:41 +0200

    home

--- typescript/ClassNotes/todoapp-typescript/src/pages/Home.tsx
+++ typescript/ClassNotes/todoapp-typescript/src/pages/Home.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import { Container, Typography } from '@mui/material';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 const Home = () => {
+  const [todos, setTodos] = useState<TodoType[]>([]);
+  const url:string = import.meta.env.VITE_BASE_URL
+
+  const getTodos = async () => {
+    try {
+      // const {data} = await axios(url)
+      const {data} = await axios<TodoType[]>(url)
+      console.log(data)
+      setTodos(data)
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(()=>{
+    getTodos()
+  },[])
+  
   return (
-    <div>Home</div>
+    <Container>
+      <Typography>Todo App with Typescript</Typography>
+    </Container>
   )
 }
 
