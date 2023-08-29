892233a20344d46ecd5a3d0c22d6e8a1f31e8d49
Umit Yildirim <umity@uia.no>
2023-08-29 22:03:00 +0200

    staged

--- nextjs/class-notes/nc-01-02/netflix-clone/app/layout.js
+++ nextjs/class-notes/nc-01-02/netflix-clone/app/layout.js
@@ -1,8 +1,10 @@
+import Navbar from "@/components/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
+//! metadata objesi ve generateMetadata fonksiyonu sadece Server Componentlerden export edilebilir.
 export const metadata = {
   title: "Netflix",
   description: "a movie platform",
@@ -11,7 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
--- nextjs/class-notes/nc-01-02/netflix-clone/app/page.js
+++ nextjs/class-notes/nc-01-02/netflix-clone/app/page.js
@@ -1,8 +1,8 @@
 import Link from "next/link";
+//? app folder içindeki page.js "/" route olan home sayfasıdır
 
-//? app folder icindeki page.js "/" route olan home sayfasidir.
+//? nextjs'de tüm componentler default olarak server componenttir. client component kullanmak için "use-client" direktifini kullanmamız gerekir
 
-//? next.jsæde tum componentler default olarak server componenttir. Client component kullanmak icin "use-client" direktifini kullanmamiz gerekir.
 export default function Home() {
   return (
     <div className="relative h-screen w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -15,9 +15,8 @@ export default function Home() {
             Watch anywhere. Cancel anytime.
           </p>
           {/* //? next/link arka planda sayfayı önceden fetch edilir. Bu, client tarafı gezintilerin performansını iyileştirmek için kullanışlıdır. Görünüm alanındaki herhangi bir <Link />  önceden yüklenecektir. */}
-
           <Link href="/register">
-            <button className="btn-danger w-[250px]">Get started</button>
+            <button className="btn-danger w-[250px]">Get Started</button>
           </Link>
         </div>
       </div>
