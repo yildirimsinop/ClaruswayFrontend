732de481371b37c8681bb4f706bd0f3af1c5f7c9
Umit Yildirim <umity@uia.no>
2023-08-31 19:44:28 +0200

    rafc

---
+++ nextjs/class-notes/nc-01-02/netflix-clone-s2-starter/app/(private)/profile/component/CardContainer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CardContainer = () => {
+  return <div></div>;
+};
+
+export default CardContainer;
--- nextjs/class-notes/nc-01-02/netflix-clone-s2-starter/app/(private)/profile/page.jsx
+++ nextjs/class-notes/nc-01-02/netflix-clone-s2-starter/app/(private)/profile/page.jsx
@@ -4,7 +4,14 @@ export const metadata = {
   title: "Profile",
 };
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className="flex items-center justify-center mt-20">
+      Profile
+      <div className="flex flex-col">
+        <h1>Who&#39;s watching?</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
