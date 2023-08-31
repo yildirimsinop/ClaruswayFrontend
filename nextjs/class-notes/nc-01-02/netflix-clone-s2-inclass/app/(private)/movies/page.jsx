ba9c1bd63577abb1668f3c84d2050dc6b68d5a24
Umit Yildirim <umity@uia.no>
2023-08-31 22:11:50 +0200

    js

--- nextjs/class-notes/nc-01-02/netflix-clone-s2-inclass/app/(private)/movies/page.jsx
+++ nextjs/class-notes/nc-01-02/netflix-clone-s2-inclass/app/(private)/movies/page.jsx
@@ -3,7 +3,9 @@ import React from "react";
 export const metadata = {
   title: "Movies",
 };
-const Movies = () => {
+const Movies = async () => {
+  const movies = getMovies("now_playing");
+  console.log(movies);
   return <div>Movies</div>;
 };
 
